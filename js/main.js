42c70c94471a202dcb05dcb7010d0ef855fcd520
Adam Oliveras <theadamoliveras@gmail.com>
2018-09-02 11:31:05 -0700

    fixed empty input field bug and fixed color changing for neg and pos bug

--- js/main.js
+++ js/main.js
@@ -1,61 +1,41 @@
-console.log('JS Loadee!!!');
-
-
-//create const for input, plus button and minus buttons
-//add eventlisteners to all 3
-//create a function that adds input
-//getinput
-//getoutput
-//add them
-//make new output
-
-//create a function that subtracts input
 function main(){
 
-const input = document.querySelector('input');
-const output = document.getElementById('output');
-const addButton = document.getElementById('plus-btn');
-const minusButton = document.getElementById('minus-btn');
-output.innerText = 0;
-
-
-addButton.addEventListener('click', function() {
-   //console.log('clicked');
-    let hNumber = output.innerText;
-    console.log(typeof(hNumber));
-    console.log(hNumber);
-    let outNum = parseInt(hNumber, 10);
-    console.log(typeof(outNum));
-    console.log(outNum);
-    let iNumber = input.value;
-    console.log(typeof(iNumber));
-    let inNum = parseInt(iNumber, 10);
-    console.log(typeof(inNum));
-    console.log(inNum);
-    output.innerText = inNum + outNum;
+    const input = document.querySelector('input');
+    const output = document.getElementById('output');
+    const addButton = document.getElementById('plus-btn');
+    const minusButton = document.getElementById('minus-btn');
+    output.innerText = 0;
+    
+    addButton.addEventListener('click', function() {
+        if (input.value === "") { 
+            alert('please select initial input value');
+        } else {
+        let hNumber = output.innerText;
+        let outNum = parseInt(hNumber, 10);
+        let iNumber = input.value;
+        let inNum = parseInt(iNumber, 10);
+        output.innerText = inNum + outNum;
+            if (output.innerText >= 0) {
+                output.removeAttribute("class");
+                }
+            }
     });
 
 
-minusButton.addEventListener('click', function() {
-    console.log("minus button clicked!");
-    let hNumber = output.innerText;
-    console.log(typeof(hNumber));
-    console.log(hNumber);
-    let outNum = parseInt(hNumber, 10);
-    console.log(typeof(outNum));
-    console.log(outNum);
-    let iNumber = input.value;
-    console.log(typeof(iNumber));
-    let inNum = parseInt(iNumber, 10);
-    console.log(typeof(inNum));
-    console.log(inNum);
-    output.innerText = outNum - inNum;
-    if (output.innerText < 0) {
-        output.className = "red";
-    }
-
-
-});
+    minusButton.addEventListener('click', function() {
+        if (input.value === "") { 
+            alert('please select initial input value');
+        } else {
+            let hNumber = output.innerText;
+            let outNum = parseInt(hNumber, 10);
+            let iNumber = input.value;
+            let inNum = parseInt(iNumber, 10);
+            output.innerText = outNum - inNum;
+                if (output.innerText < 0) {
+                    output.className = "red";
+            }
+        }
+    });
 
 
 };
